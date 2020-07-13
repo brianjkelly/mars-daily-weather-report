8c99c10244e794bbe8dd302a8f690dc63cb7f020
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-07-13 15:40:25 -0700

    created promise for data retrieval

--- js/script.js
+++ js/script.js
@@ -6,9 +6,33 @@ let weatherData;
 
 /*----- cached element references -----*/
 
-const $date = $('#date')
-const $sol = $('#sol')
+const $date = $('#date');
+const $sol = $('#sol');
+const $season = $('#season');
+const $maxTemp = $('#maxTemp');
+const $avgTemp = $('#avgTemp');
+const $minTemp = $('#minTemp');
+const $maxWind = $('#maxWind');
+const $avgWind = $('#avgWind');
+const $minWind = $('#minWind');
+const $dirWind = $('#dirWind');
+const $maxPa = $('#maxPa');
+const $avgPa = $('#avgPa');
+const $minPa = $('#minPa');
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+
+function handleGetData() {
+    $.ajax({
+        url:'https://api.nasa.gov/insight_weather/?api_key=p8oOGx28D5rYdVoINo3rcPBlOBXpWjI4AcexcORz&feedtype=json&ver=1.0'
+    }).then(
+        (data) => {
+            console.log(data);
+        },
+        (error) => {
+            console.log('bad request: ', error);
+        }
+    )
+};
