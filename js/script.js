ea48d13017d6cc313d370eab56f1e2a89ffbe529
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-07-13 15:32:30 -0700

    defined variables and element references

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 
 /*----- app's state (variables) -----*/
 
+let weatherData;
+
 /*----- cached element references -----*/
+
+const $date = $('#date')
+const $sol = $('#sol')
 
 /*----- event listeners -----*/
 
